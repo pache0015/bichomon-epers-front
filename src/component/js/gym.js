b77c12e99025206818a3cfe7b43c22b8a6ac873c
Lucas Wojciechow <lucas.wojciechow@10pines.com>
2019-11-21 19:34:47 -0300

    arreglando add modal y reset all

--- src/component/js/gym.js
+++ src/component/js/gym.js
@@ -5,9 +5,6 @@ class Gym extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            nombre: '',
-            bicho: '',
-            entrenador:'',
             error: 'No hay dato',
         }
 
@@ -17,9 +14,9 @@ class Gym extends React.Component{
             <div className="gym-container">
                 <h1>{this.props.nombre}</h1>
                 <div className="info-gym">
-                    <label className="info">Nombre: {this.state.nombre}</label>
-                    <label className="info">Bicho campeon: {this.state.bicho}</label>
-                    <label className="info">Entrenador campeon: {this.state.entrenador}</label>
+                    <label className="info">Nombre: {this.props.nombre}</label>
+                    <label className="info">Bicho campeon: {this.props.bicho}</label>
+                    <label className="info">Entrenador campeon: {this.props.entrenador}</label>
 
                 </div>
 
