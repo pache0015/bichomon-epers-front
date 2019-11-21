c9592c8e36c9fe215b759a7ad05c03813b99e9ec
pache0015 <pache.facundo@gmail.com>
2019-11-21 19:25:27 -0300

    Se termino componente Login

--- src/component/js/Login.js
+++ src/component/js/Login.js
@@ -2,33 +2,51 @@ import React from 'react';
 import '../css/Login.css'
 
 class Login extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            error:'',
+            user:'',
+        };
+        this.checkInput = this.checkInput.bind(this);
+        this.handleUser = this.handleUser.bind(this);
+        this.logUser = this.logUser.bind(this);
+        this.redirectToNextPage = this.redirectToNextPage.bind(this);
+    }
+    handleUser(event){
+        this.setState({user: event.target.value})
+    }
+    checkInput = (e) => {
+        e.preventDefault();
+        this.setState({error: ""});
+        if (this.state.user.trim().length < 1) {
+            this.setState({error: "Campos vacios..."});
+            return;
+        }
+        this.logUser()
+    };
+
+    logUser = () =>{
+        localStorage.setItem('user', this.state.user)
+        this.redirectToNextPage()
+    }
+    redirectToNextPage = () =>{
+        this.props.history.push('/app')
+    }
     render() {
         return(
             <div className="log-container">
                 <h1>Logueate</h1>
                 <form className="formLog">
                     <div className="inputContainer">
-                    <label className="titleInput">User</label>
-                    <input className="logInput"/>
+                        <input className="logInput" placeholder="Username" value={this.state.user} onChange={this.handleUser}/>
                     </div>
-                    <div className="inputContainer">
-                    <label className="titleInput">Password</label>
-                    <input className="logInput" type="password"/>
-                    </div>
-                    <button type="submit" value="submit" className="btnLog">
-                        Ingresa con tu cuenta de Facebook
-                    </button>
-                    <button type="button" value="submit" className="btnLog">
-                        Ingresa con tu cuenta de Google
-                    </button>
-                    <button type="button" value="submit" className="btnLog">
-                        Login
-                    </button>
-                    <button type="submit" value="submit" className="btnLog">
-                        Register
+                    <button type="submit" value="submit" className="btnLog" onClick={this.checkInput}>
+                        Ingresar
                     </button>
 
                 </form>
+                <label className="errorLog">{this.state.error}</label>
             </div>
         )
     }
