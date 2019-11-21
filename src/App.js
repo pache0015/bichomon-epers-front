2c13db36e89563f8e8b9a7c70a31c7a3857f24c4
pache0015 <pache.facundo@gmail.com>
2019-11-21 18:12:10 -0300

    se agregaron imports de firebase

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React from 'react';
 import './App.css';
 import MapWorld from "./component/js/MapWorld";
 import NotificationContainer from "./component/js/NotificationContainer";
+import firebase from 'firebase/app';
+import {configDB} from "./config/config";
+import 'firebase/database';
 
 function App() {
+
+  this.app = firebase.initializeApp(configDB);
+  this.db = this.app.database().ref().child('vilmaPalma')
   return (
     <div className="App">
 
