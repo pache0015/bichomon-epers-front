3e10fec55e0a2aa9d83c6a20758572453cef2afa
pache0015 <pache.facundo@gmail.com>
2019-11-22 19:53:19 -0300

    notifications wip 2.0

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
         super(props);
         this.state = {
             showModal: false,
-            showModalLogin: true,   
+            showModalLogin: true,
             entrenador: '',
             bicho: '',
             gimnasio: '',
--- src/component/js/NotificationContainer.js
+++ src/component/js/NotificationContainer.js
@@ -2,33 +2,65 @@ import React from 'react';
 import '../css/NotificationContainer.css';
 import Notification from "./Notification";
 import * as firebase from "firebase/app";
+import Gym from "./gym";
 
 
 class NotificationContainer extends React.Component{
     constructor(props) {
-        super(props)
-        this.handleResetAll = this.handleResetAll.bind(this)
+        super(props);
+        this.state = {
+            notifications: '',
+        }
+        this.handleResetAll = this.handleResetAll.bind(this);
+        this.changeChampion =  this.changeChampion.bind(this);
+        this.pullNotification = this.pullNotification.bind(this);
+
+
+        //=======> ESTO HAY QUE SACARLO DESPUES <==========
+
+        localStorage.setItem('gym', 'un gym piola');
+
     }
     handleResetAll() {
         if(window.confirm("Tas seguro ameo?")) {
             firebase.database().ref('gyms').set({});
         }
     }
+    changeChampion(value){
+
+        firebase.database().ref('notification').push({
+            usuario: localStorage.getItem('user'),
+            accion: value,
+            gym: localStorage.getItem('gym')
+        }, () => this.pullNotification())
+    };
+    pullNotification(){
+        const notsRef = firebase.database().ref('notification');
+        nostRef.on('value', (snapshot) => {
+            const notifications = snapshot.val() ? Object.values(snapshot.val()) : []
+            this.setState({notification:notifications})
+        })
+
+    }
 
     render() {
+        const notificationComponents = this.pullNotification.map((notification, i) => {
+            return <Notification entrenador={notification.usuario} textNotification={notification.accion} gym={notification.gym} key={i}/>
+        })
         return(
             <div className="not-container">
                 <div className="controller-container">
                     <h1>Buttons</h1>
                     <input className="gym-selected" placeholder="gym selected"/>
-                    <button className="controller">Change champion</button>
-                    <button className="controller" onClick={this.handleResetAll}>Reset ALL</button>
-                    <button className="controller" onClick={this.props.modalCallback}>Add Gym</button>
+                    <button className="controller" value={"cambio campeon"} onClick={() => changeChampion(value())}>Change champion</button>
+                    <button className="controller" value={"reseteo todo"} onClick={this.handleResetAll}>Reset ALL</button>
+                    <button className="controller" value={"añadio un gym"} onClick={this.props.modalCallback}>Add Gym</button>
                 </div>
                 <div className="nots">
                     <h1>Notifications</h1>
-                    <Notification entrenador="Ash " textNotification="fue campeon en " gym="lala"/>
-                    <Notification textNotification="Estoy probando el tamaño de esta notificacion"/>
+                    {notificationComponents}
+
+
                 </div>
             </div>
         )
